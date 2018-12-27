da99551d8db581a23e38af52e6163be083e6b7ff
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2018-12-27 10:55:58 +0100

    react-router-dom installed

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
-
+import { BrowserRouter, Router } from 'react-router-dom'
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar></Navbar>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar/>
+          <Router path='/' component={Home}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
