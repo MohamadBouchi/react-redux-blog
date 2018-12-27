73bb05b7f1d5ebb9fcfba28cf84d14fb470c8da6
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2018-12-27 11:04:30 +0100

    navbar link added

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
-import { BrowserRouter, Router } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './Components/Home'
 import About from './Components/About'
 import Contact from './Components/Contact'
@@ -11,7 +11,9 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar/>
-          <Router path='/' component={Home}/>
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/contact' component={Contact}/>
         </div>
       </BrowserRouter>
     );
--- blog/src/Components/Navbar.js
+++ blog/src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
@@ -6,9 +7,9 @@ export default function Navbar() {
         <div className="container">
             <a className='brand-logo'>My Times</a>
             <ul className='right'>
-                <li><a href='/'>Home</a></li>
-                <li><a href='/about'>About</a></li>
-                <li><a href='/contact'>Contact</a></li>
+                <li><NavLink to='/'>Home</NavLink></li>
+                <li><NavLink to='/about'>About</NavLink></li>
+                <li><NavLink to='/contact'>Contact</NavLink></li>
             </ul>
         </div>
     </nav>
