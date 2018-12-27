e14a46aae363c2e4e65cd020b8ab018d06cd96d3
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2018-12-27 10:52:20 +0100

    components created

---
+++ blog/src/Components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure velit eaque nulla, eum vero distinctio, sit blanditiis mollitia soluta quasi officiis ex quaerat voluptatum a quam, totam dolores deserunt placeat!</p>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ blog/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure velit eaque nulla, eum vero distinctio, sit blanditiis mollitia soluta quasi officiis ex quaerat voluptatum a quam, totam dolores deserunt placeat!</p>
+        </div>
+    )
+}
+
+export default Home;
